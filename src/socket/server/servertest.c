0bdf44ed956fab771562491fa0ea112da6d8260e
Geurney <Geurney@users.noreply.github.com>
2015-04-30 23:18:59 -0700

    Update servertest

--- src/socket/server/servertest.c
+++ src/socket/server/servertest.c
@@ -1,15 +1,14 @@
-#include "client.h"
-
+/*
+ * runserver.c
+ *
+ *  Created on: Apr 30, 2015
+ *      Author: geurney
+ */
+#include "server.h"
 int main(int argc, char *argv[]) {
-	char* hostname;
-	char* matrix;
-	char result[READBUFFERSIZE];
+	char* result = (char*)malloc(MATRIXMAXSIZE*sizeof(char));
 
-	hostname = "localhost";
-	matrix = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16";
-
-	if(sendCounterExample(hostname, SERVERPORT, matrix, result)) {
-		printf("%s\n", result);
-	}
+	receiveCounterExample(result);
+	free(result);
 	return 0;
 }
