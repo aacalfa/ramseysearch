beb89872a898b431750061d76cd49725d0beda3c
Geurney <Geurney@users.noreply.github.com>
2015-05-01 19:41:35 -0700

    Update convert gsize to string

--- src/socket/client/clienttest.c
+++ src/socket/client/clienttest.c
@@ -1,19 +1,21 @@
 #include "client.h"
 #include "graph_utils.h"
+#include "format.h"
 int main(int argc, char *argv[]) {
 	char* hostname;
 	char* matrix;
 	char result[READBUFFERSIZE];
 	int *g;
 	int gsize;
+	char* size;
 
 	hostname = "localhost";
 //	matrix = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16";
 
 	ReadGraph("n8.txt", &g, &gsize);
         matrix = GraphtoChar(g,gsize);
-
-	if(sendCounterExample(hostname, SERVERPORT, matrix, gsize, result)) {
+        size = tostring(gsize);
+	if(sendCounterExample(hostname, SERVERPORT, matrix, size, result)) {
 		printf("%s\n", result);
 	}
 	free(matrix);
