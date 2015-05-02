a291ed7c18e2bc310c4aa7d96075684d0bfddde5
Geurney <Geurney@users.noreply.github.com>
2015-05-01 19:39:15 -0700

    Add format.h

---
+++ src/simple_taboo_search/format.h
@@ -0,0 +1,14 @@
+/*
+ * format.h
+ *
+ *  Created on: May 1, 2015
+ *      Author: geurney
+ */
+
+#ifndef FORMAT_H_
+#define FORMAT_H_
+#include <stdlib.h>
+
+char* tostring(int num);
+
+#endif /* FORMAT_H_ */
