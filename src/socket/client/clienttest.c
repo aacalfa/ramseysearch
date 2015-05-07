4d8299717ebf2594ca1ea358264d208ef89ff30c
Geurney <Geurney@users.noreply.github.com>
2015-05-07 11:06:55 -0700

    Update send result interface
    Add clique counter into the message

--- src/socket/client/clienttest.c
+++ src/socket/client/clienttest.c
@@ -7,20 +7,26 @@ int main(int argc, char *argv[]) {
 	char feedback[READBUFFERSIZE];
 	int *g;
 	int gsize;
+	int gccounter;
 //	char  size[3];
 	char* size;
-
+	char* counter;
+	
 	hostname = "localhost";
 //	matrix = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16";
 
 	ReadGraph("n109.txt", &g, &gsize);
         matrix = GraphtoChar(g,gsize);
+        gccounter = 9;
+        
         size = NumtoString(gsize);
+        counter = NumtoString(gccounter);
+        
 //	sprintf(size, "%d", gsize);
 	//printf("%s",size);
 
 	bzero(feedback, READBUFFERSIZE);
-	if(sendResult(hostname, SERVERPORT, COUNTEREXAMPLE, matrix, size, feedback)) {
+	if(sendResult(hostname, SERVERPORT, matrix, size, counter, feedback)) {
 		printf("%s\n", feedback);
 	}
 	free(matrix);
