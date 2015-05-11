396dc92c6ed4317d2cec5df97eb4b6656c7a4b35
aacalfa <aacalfa@gmail.com>
2015-05-10 17:39:51 -0700

    Forgot to add msg.h

---
+++ src/utils/msg.h
@@ -0,0 +1,21 @@
+#ifndef MSG_H_
+#define MSG_H_
+
+/*
+* The size of read buffer. As large as the matrix size.
+*/
+#define READBUFFERSIZE 220*220
+
+/*
+* Fixed Server Port.
+*/
+#define SERVERPORT 5098
+
+/*
+* Flag of message.
+*/
+#define RESULT '0'
+#define REQUEST '1'
+#define DENY '2'
+
+#endif /* MSG_H_ */
