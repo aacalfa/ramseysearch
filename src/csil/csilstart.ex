3dc01d0cbd55404165b5f91c5253dc2cd00df1c5
Geurney <Geurney@users.noreply.github.com>
2015-05-22 19:23:25 -0700

    Start Csil machines

---
+++ src/csil/csilstart.ex
@@ -0,0 +1,24 @@
+#!/usr/bin/expect
+set count 0
+set f [open "csil.txt"]
+set hosts [split [read $f] "\n"]
+close $f
+
+# Iterate over the hosts
+foreach host $hosts {
+	if {$host == "EOF"} {
+	    break;
+	} else {
+		incr count 1
+		puts "$count: $host"
+		set timeout 20
+		spawn ssh $host
+		expect "$ "
+		send "cd bettertaboo/src/simple_taboo_search\r"
+		expect "$ "
+		send "nohup ./simple_taboo_search-7 &\r"	
+		send "exit\r"
+		expect "$ "
+		puts "\n"
+	}
+}
